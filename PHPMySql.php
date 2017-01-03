80d48213fde375b15f7a7d93f674811b08037908
ecolobo <ecolobo@gmail.com>
2017-01-02 22:49:47 -0200

    Update PHPMySql.php

--- PHPMySql.php
+++ PHPMySql.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "146.148.107.234:3306";
+$servername = "146.148.107.234";
 $username = "root";
 $password = "Escudo2430!";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=guestbook", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
