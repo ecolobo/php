97abc539e45e6ae0419ebd2d9c4d1c5f66f84da0
ecolobo <ecolobo@gmail.com>
2017-01-03 10:56:22 -0200

    Update PHPMySql.php

--- PHPMySql.php
+++ PHPMySql.php
@@ -2,6 +2,6 @@
 $host = '146.148.107.234';
 $username = 'daniel_lobo';
 $password= 'Escudo2430!';
-$conStr = "firebird:dbname={$host};User={$username};Password={$password};Port=3050;"; 
+$conStr = "firebird:dbname={$host};User={$username};Password={$password};Port=3306;"; 
 $DB = new PDO($conStr);
 ?>
