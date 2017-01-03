1ed20db3607c39914cba1ec839fa83476eea25b4
ecolobo <ecolobo@gmail.com>
2017-01-03 09:02:08 -0200

    Update PHPMySql.php

--- PHPMySql.php
+++ PHPMySql.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "146.148.107.234:3306";
-$username = "root";
-$password = "Escudo2430!";
-$db = "guestbook";
+$link = mysqli_connect("146.148.107.234", "root", "Escudo2430!", "guestbook");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 ?>
