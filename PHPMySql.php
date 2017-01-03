43a86fcc83043d3518a2ae2d71e7e205d0d3a31e
ecolobo <ecolobo@gmail.com>
2017-01-03 08:40:53 -0200

    Update PHPMySql.php

--- PHPMySql.php
+++ PHPMySql.php
@@ -1,26 +1,9 @@
+
 <?php
-    if(!$link = mysql_connect("146.148.107.234", "root", "Escudo2430!")) {
-        echo "Não foi possível conectar ao mysql";
-        exit;
-    }// fim do if(!$link = mysql_connect('146.148.107.234', 'root', 'Escudo2430!'))
-
-    if(!mysql_select_db('guestbook', $link)) {
-        echo 'Não foi possível selecionar o banco de dados';
-        exit;
-    }// fim do if(!mysql_select_db('guestbook', $link))
-
-    $sql = 'SELECT * FROM farma';
-    $result = mysql_query($sql, $link);
-
-    if(!$result) {
-        echo "Erro do banco de dados, não foi possível consultar o banco de dados\n";
-        echo 'Erro MySQL: '.mysql_error();
-        exit;
-    }// fim do if(!$result)
-
-    while($row = mysql_fetch_assoc($result)) {
-        echo $row['cidade'];
-    }// fim do while($row = mysql_fetch_assoc($result))
-
-    mysql_free_result($result);
+$link = mysql_connect('146.148.107.234', 'root', 'Escudo2430!');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 ?>
